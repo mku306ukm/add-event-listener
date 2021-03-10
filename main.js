72e9601bab5aae671d562119d43f5cd77dafd6dc
samod singh <samod0098@gmail.com>
2021-03-10 22:33:30 +0530

    add-event-listener

---
+++ main.js
@@ -0,0 +1,17 @@
+var element;
+
+
+
+document.getElementById("header").addEventListener("mouseenter",abc);
+document.getElementById("header").addEventListener("click",function(){
+    document.getElementById("header").style.border = "10px solid red"
+});
+
+
+function abc(){
+    document.getElementById("header").style.background = "green";  
+}
+
+
+
+// console.log(element);
